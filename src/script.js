d5759751eda5bf65a30f9df6d2c183f4b4f0fec6
Tekfred <asagodfred101@gmail.com>
2025-11-28 11:26:15 +0000

    updated script and modified design

--- src/script.js
+++ src/script.js
@@ -1,4 +1,20 @@
-var toggle = document.getElementById("toggle");
-toggle.addEventListener("click", function(){
-    this.innerHTML = ''
+
+
+
+var lightmode = document.getElementById("light");
+var Darkmode = document.getElementById("dark");
+
+
+document.getElementById("toggle").addEventListener('click', function(){
+    lightmode.classList.toggle("bg-[#7380ec]");
+    lightmode.classList.toggle("text-white");
+    lightmode.classList.toggle("bg-[#d0d4fa]");
+
+    Darkmode.classList.toggle('bg-[#7380ec]');
+    Darkmode.classList.toggle('text-white');
+    Darkmode.classList.toggle('bg-[#d0d4fa]');
+    Darkmode.classList.toggle('translate-x-[-3px]');
+    Darkmode.classList.toggle('rounded-l-md');
+    
+    
 })
